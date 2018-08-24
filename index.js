41bf518e8e43d03bef4b6b6c7a902e13cb80e747
ErinHannan <erinehannan@gmail.com>
2018-08-24 00:31:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 window.onload = function(e) {
   console.log("window.onload");
   
-  c
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+ 
+function init() {
+  console.log("== init ==");
+  
+  let bodyEl = document.getElementsByTagName("body");
+  console.log("bodyEl;" , bodyEl);
+  
+  bodyArray[0].addEventListener("keydown", onKeyDownHandler);
+  
+  function onKeyDownHandler() {
+    console.log("== onKeyDownHandler ==");
+  }
+}
+init();
 }
